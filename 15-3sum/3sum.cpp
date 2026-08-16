be72e4e004f7b98d59e32d39bdb604deac5820b2
Iamstrong03 <tanishkagrawal9882@gmail.com>
2026-08-16 15:12:47 +0530

    Time: 41 ms (87.95%) | Memory: 29 MB (73.37%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -2,34 +2,38 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> v;
-        vector<vector<int>> ans;
         int n = nums.size();
         sort(nums.begin(), nums.end());
         // -4, -1, -1 , 0, 1, 2
         for(int i = 0; i < n - 2; i++) {
-            for(int k = 0; k < i; k++) {
-                if(nums[k] == nums[i] && i < n - 2) i++;
+            if(nums[i] > 0) {
+                break;
             }
-            int f = nums[i];  //-4
+            if(i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+
             int l = i + 1;
             int r = n - 1;
             while(l < r) {
-                if(f + nums[l] + nums[r] == 0) {
-                    v.push_back({f, nums[l], nums[r]});
+                int sum = nums[i] + nums[l] + nums[r];
+                if(sum == 0) {
+                    v.push_back({nums[i], nums[l], nums[r]});
                     l++;
                     r--;
+                    // remove duplicates
+                    while(l < r && nums[l] == nums[l-1]){
+                        l++;
+                    }
+                    while(l < r && nums[r] == nums[r + 1]){
+                        r--;
+                    }
                 }
-                else if(f + nums[l] + nums[r] > 0) r--;
+                else if(sum > 0) r--;
                 else l++;
             }
         }
-        set<vector<int>> s;
-        for (auto x : v) {
-            s.insert(x);
-        }
-        for(auto x : s) {
-            ans.push_back(x);
-        }
-        return ans;
+        return v;
     }
 };
+
