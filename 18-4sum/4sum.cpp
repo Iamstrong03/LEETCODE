b44cc7e035e26c16c834eae2dd647c8e5281b2dc
Iamstrong03 <tanishkagrawal9882@gmail.com>
2026-08-25 18:55:29 +0530

    Time: 23 ms (53.97%) | Memory: 17.5 MB (56.81%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -3,30 +3,28 @@ public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n = nums.size();
         vector<vector<int>> v;
-        set<vector<int>> s;
         sort(nums.begin(), nums.end());
 
         for(int i = 0; i < n - 3; i++) {
+            if(i > 0 && nums[i] == nums[i-1]) continue;
             for(int j = i + 1; j < n - 2; j++) {
+                if(j != i + 1 && nums[j] == nums[j - 1]) continue;
                 int l = j + 1;
                 int r = n - 1;
                 while(l < r) {
-                    long long sum1 = nums[i] + nums[j];
-                    long long sum2 = nums[l] + nums[r];
-                    if(sum1 + sum2 == target) {
-                        s.insert({nums[i], nums[j], nums[l], nums[r]});
-                        l++;
-                        r--;
+                    long long sum = nums[i] + nums[j];
+                    sum += nums[l] + nums[r];
+                    if(sum == target) {
+                        vector<int> temp = {nums[i], nums[j], nums[l], nums[r]};
+                        v.push_back(temp);
+                        l++; r--;
+                        while(l < r && nums[l] == nums[l - 1]) l++;
+                        while(l < r && nums[r] == nums[r + 1]) r--;
                     }
-                    else if(sum1 + sum2 > target) {
-                        r--;
-                    }
+                    else if(sum > target) r--;
                     else l++;
                 }
             }
-        }
-        for(auto x : s) {
-            v.push_back(x);
         }
         return v;
     }
