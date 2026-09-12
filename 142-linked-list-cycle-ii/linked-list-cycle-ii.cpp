091d285b82e912ff0d7c09d3e4988a6b6b340bba
Iamstrong03 <tanishkagrawal9882@gmail.com>
2026-09-12 15:01:40 +0530

    Time: 6 ms (71.47%) | Memory: 11.4 MB (54.70%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -12,14 +12,19 @@ public:
         if(head == NULL || head->next == NULL) {
             return NULL;
         }
-        ListNode* temp =  head;
-        map<ListNode*, int> mpp;
-        while(temp != NULL)  {
-            if(mpp.find(temp) != mpp.end()) {
-                return temp;
+        ListNode* slow =  head;
+        ListNode* fast = head;
+        while(fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast) {
+                slow = head;
+                while(slow != fast)  {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
             }
-            mpp[temp] = 1;
-            temp = temp->next;
         }
         return NULL;
     }
