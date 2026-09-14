5410589554db4883aa22da202d2c0b76672d2adb
Iamstrong03 <tanishkagrawal9882@gmail.com>
2026-09-14 16:27:58 +0530

    Time: 39 ms (72.41%) | Memory: 84.7 MB (83.48%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& nums) {
+        int n = nums.size();
+        int l = 0;
+        int maxans = 0;
+        unordered_map<int, int> mpp;
+        for(int i = 0; i < n; i++) {
+            mpp[nums[i]]++;
+            if(mpp.size() <= 2) {
+                maxans = max(maxans, i - l + 1);
+            }
+            else {
+                while(mpp.size() > 2) {
+                    mpp[nums[l]]--;
+                    if(mpp[nums[l]] == 0) mpp.erase(nums[l]);
+                    l++;
+                }
+            }
+        }
+        return maxans;
+    }
+};
